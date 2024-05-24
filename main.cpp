46d8b43810762ac64290c2e2d43d7caaac325845
Fomka <striker2507@gmail.com>
2024-05-24 08:27:10 +0300

    Figured out how to draw lines by two points

--- main.cpp
+++ main.cpp
@@ -2,40 +2,36 @@
 #include <stdlib.h>
 #include <math.h>
 #include "matplotlibcpp.h"
+#include <iostream>
+#include <cmath>
 #include <vector>
 
 namespace plt = matplotlibcpp;
+// namespace pyplt = matplotlibcpp::
 
 const int K = 7;
 const int K1 = 8;
 const int N = 11;
 
-void gaussMethod(double c1[K1][K1], double d1[K1], double b[K1])
+// void gaussMethod(double c1[K1][K1], double d1[K1], double b[K1])
+void gaussMethod(std::vector<std::vector<double>> &c, std::vector<double> &d, std::vector<double> &b)
 {
     double q[K1][K1];
     int indD[K1];
     int cold[K1];
 
-    // зануление
     for (int i = 0; i < K1; ++i)
     {
         indD[i] = i;
     }
 
-    // printf("\nD1!!!!!!!\n");
-    // for (int kok = 0; kok < K1; ++kok) {
-    //     printf("%f\t", d1[kok]);
-    // }
-    // printf("\n\n");
     // reducing coefficient matrix to a triangular one
-
-    // Приводим матрицу коэффициентов к треугольной
     for (int i = 0; i < K; ++i)
     {
         int i0 = i;
         int iflag = -1;
         int j1 = i + 1;
-        double cmax = c1[i][i];
+        double cmax = c[i][i];
 
         // массив не меняется
         // ищется максимум (cmax)
@@ -45,10 +41,10 @@ void gaussMethod(double c1[K1][K1], double d1[K1], double b[K1])
         // НИХУЯ НЕ ДЕЛАЕТ
         for (int j = j1; j < K1; ++j)
         {
-            if (fabs(cmax) < fabs(c1[j][i]))
+            if (fabs(cmax) < fabs(c[j][i]))
             {
                 i0 = j;
-                cmax = c1[j][i];
+                cmax = c[j][i];
                 iflag = 1;
             }
         }
@@ -60,13 +56,13 @@ void gaussMethod(double c1[K1][K1], double d1[K1], double b[K1])
             printf("iflag test\n");
             for (int j = 0; j < K1; ++j)
             {
-                cold[j] = c1[i][j];
-                c1[i][j] = c1[i0][j];
-                c1[i0][j] = cold[j];
+                cold[j] = c[i][j];
+                c[i][j] = c[i0][j];
+                c[i0][j] = cold[j];
             }
-            double dold = d1[i];
-            d1[i] = d1[i0];
-            d1[i0] = dold;
+            double dold = d[i];
+            d[i] = d[i0];
+            d[i0] = dold;
 
             double indDold = indD[i];
             indD[i] = indD[i0];
@@ -77,43 +73,27 @@ void gaussMethod(double c1[K1][K1], double d1[K1], double b[K1])
         // массив меняется здесь
         for (int j = j1; j < K1; ++j)
         {
-            q[j][i] = c1[j][i] / c1[i][i];
-            d1[j] -= q[j][i] * d1[i];
+            q[j][i] = c[j][i] / c[i][i];
+            d[j] -= q[j][i] * d[i];
             for (int j2 = j1; j2 < K1; ++j2)
             {
-                c1[j][j2] -= q[j][i] * c1[i][j2];
+                c[j][j2] -= q[j][i] * c[i][j2];
             }
-            // printf("\nMATRIX IN MNK\ni = %d, j = %d\n", i, j);
-            // for (int hui = 0; hui < K1; ++hui)
-            // {
-            //     for (int jopa = 0; jopa < K1; ++jopa)
-            //     {
-            //         printf("%f\t", c1[hui][jopa]);
-            //     }
-            //     printf("\n");
-            // }
         }
     }
 
-    // printf("\nD1!!!!!!!\n");
-    // for (int kok = 0; kok < K1; ++kok) {
-    //     printf("%f\t", d1[kok]);
-    // }
-    // printf("\n");
-
-
-    b[K1 - 1] = d1[K1 - 1] / c1[K1 - 1][K1 - 1];
+    b[K1 - 1] = d[K1 - 1] / c[K1 - 1][K1 - 1];
 
     for (int i = 0; i < K; ++i)
     {
         int IM = K - i - 1;
-        double aux = d1[IM];
+        double aux = d[IM];
         for (int j = IM; j < K; ++j)
         {
-            aux -= c1[IM][j + 1] * b[j + 1];
+            aux -= c[IM][j + 1] * b[j + 1];
         }
 
-        b[IM] = aux / c1[IM][IM];
+        b[IM] = aux / c[IM][IM];
     }
 }
 
@@ -121,39 +101,21 @@ int main()
 {
     int K0 = 0;
 
-    double Z[K1];
-    double D[K1];
-    double C[K1][K1];
+    std::vector<double> coefVec(K1);
+    std::vector<double> D(K1);
+    std::vector<int> NI(K);
+    std::vector<double> sumdy(K);
 
-    int NI[K];
-    double sumdy[K];
-
-    double YC[K][N];
-    double dyc[K][N];
-    double X[K][N];
-    double Y[K][N];
-
-    for (int i = 0; i < K; ++i)
-    {
-        NI[i] = 0;
-        sumdy[i] = 0;
-        for (int j = 0; j < N; ++j)
-        {
-            X[i][j] = 0;
-            Y[i][j] = 0;
-            YC[i][j] = 0;
-            dyc[i][j] = 0;
-        }
-    }
-    for (int i = 0; i < K1; ++i)
-    {
-        D[i] = 0;
-        Z[i] = 0;
-        for (int j = 0; j < K1; ++j)
-        {
-            C[i][j] = 0;
-        }
-    }
+    // double C[K1][K1]
+    // double YC[K][N];
+    // double dyc[K][N];
+    // double X[K][N];
+    // double Y[K][N];
+    std::vector<std::vector<double>> C(K1, std::vector<double>(K1));
+    std::vector<std::vector<double>> YC(K, std::vector<double>(N));
+    std::vector<std::vector<double>> dyc(K, std::vector<double>(N));
+    std::vector<std::vector<double>> X(K, std::vector<double>(N));
+    std::vector<std::vector<double>> Y(K, std::vector<double>(N));
 
     FILE *inp_file;
     if ((inp_file = fopen("OTa_q_gg_inp.dat", "r")) == NULL)
@@ -164,17 +126,8 @@ int main()
 
     fscanf(inp_file, "%d", &K0);
 
-    std::vector<std::vector<double>> iks(K, std::vector<double>(N));
-    std::vector<std::vector<double>> igrek(K, std::vector<double>(N));
-
-    // for (int i = 0; i < K; ++i) {
-    //     for (int j = 0; j < N; ++j) {
-    //         iks[i][j] = X[i][j];
-    //         igrek[i][j] = Y[i][j];
-    //     }
-    // }
-
     int NII = 0;
+    
 
     if (K0 <= K)
     {
@@ -182,6 +135,8 @@ int main()
         {
             fscanf(inp_file, "%d", &NI[i]);
         }
+
+        std::cout<< "123" << std::endl;
 
         for (int i = 0; i < K0; ++i)
         {
@@ -195,14 +150,9 @@ int main()
 
             // here X[i][j] is the Q_gg value of isotope j, element i
             // and Y[i][j] is the value of cross section's logarithm (called "Sigma") of isotope j, element i
-
-            // Здесь X[i][j] - значение величины Q_gg j-ого изотопа i-ого элемента
-            // и Y[i][j] - значение логарифма сечения "Сигма" j-ого изотопа i-ого элемента
             for (int j = 0; j < NII; ++j)
             {
                 fscanf(inp_file, "%lf %lf", &X[i][j], &Y[i][j]);
-                iks[i][j] = X[i][j];
-                igrek[i][j] = Y[i][j];
             }
         }
     }
@@ -212,18 +162,13 @@ int main()
         exit(1);
     }
 
+    
+
     fclose(inp_file);
 
-    plt::plot(iks.at(0).at(0));
-    plt::show();
-
-    // With given elements' values Q_gg and Sigma (cross section log)
+    // With given elements' values Q_gg and Sigma (cross-section log)
     // we calculate matrix elements values that are found by using
     // Least Squares method to our data
-
-    // По известным значениям величин Q_gg и Сигма
-    // находим элементы матрицы которые получаются применением
-    // метода наименьших квадратов к исходным данным
     for (int i = 1; i < K1; ++i)
     {
         NII = NI[i - 1];
@@ -249,7 +194,7 @@ int main()
         printf("\n");
     }
 
-    gaussMethod(C, D, Z);
+    gaussMethod(C, D, coefVec);
 
     FILE *out_file;
     if ((out_file = fopen("OTa_q_gg_out.dat", "w")) == NULL)
@@ -260,19 +205,19 @@ int main()
 
     for (int i = 0; i < K1; ++i)
     {
-        fprintf(out_file, " \t %d\t %.17g\n", i + 1, Z[i]);
+        fprintf(out_file, " \t %d\t %.17g\n", i + 1, coefVec[i]);
     }
 
     fprintf(out_file, "\n");
 
-    double a = Z[0];
+    double a = coefVec[0];
 
     // solving the y = ax + b equation
 
     // решаем уравнение y = ax + b
     for (int i = 0; i < K0; ++i)
     {
-        double b = Z[i + 1];
+        double b = coefVec[i + 1];
         sumdy[i] = 0.0;
         for (int j = 0; j < NI[i]; ++j)
         {
@@ -293,7 +238,28 @@ int main()
 
     fclose(out_file);
 
-    printf("\nwe are back\n");
+    std::vector<std::string> colorVec {"b", "r", "g", "c", "m", "y", "k"};
+
+    for (int i = 0; i < X.size(); ++i) {
+        for (int j = 0; j < X.size(); ++j) {
+            plt::scatter(X[i], Y[i], 40);
+        }
+    }
+
+    plt::xlim(-55, 5);
+    plt::ylim(-20, 15);
+
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {622.226, -605.471}, "b"); // 8.37
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {628.855, -598.842}, "r"); // 15.00
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {619.517, -608.179}, "g"); // 5.67
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {618.458, -609.238}, "c"); // 4.61
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {616.405, -611.291}, "m"); // 2.56
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {615.13, -612.566}, "y");  // 1.28
+    plt::plot(std::vector<int> {2000, -2000}, std::vector<double> {613.301, -614.395}, "k"); // -0.55
+    
+    plt::show();
+
+    printf("we are back\n");
 
     return 0;
 }
